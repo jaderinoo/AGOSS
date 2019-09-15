81441935023f6c041ac44714e7e4431c96e2930f
Jad El-Khatib <jaderinoo@gmail.com>
2019-09-14 20:34:49 -0400

    Finished attack move code

--- AGOSS/src/Adventure.java
+++ AGOSS/src/Adventure.java
@@ -40,20 +40,87 @@ public class Adventure {
 			System.out.println("LVL: " + attacker.getLevel() + "\t\t\t|\n"
 					+ "--------------------------------------------------");
 			int attackerDamage = 0;
+			int attackDamage = 0;
 			int damageTaken = 0;
+			int damageDealt = 0;
 			System.out.println("What would you like to do?: \n1:Attack   2:Defend "
 														 + "\n3:Bag      4:Run");
 			
 			int selection = scanner.nextInt();
 			switch(selection) {
 				case 1:
-					//Miss
-					if(ThreadLocalRandom.current().nextInt(0, 6) == 0){
-						System.out.println("\n\t\t\t|MISS");
-						break;
-					}	
+
+					//If the player is faster
+					if(player.getAgility() >= attacker.getAgility()) {
+						//System.out.println("Players Move!");
+						//Miss
+						if(ThreadLocalRandom.current().nextInt(0, 6) == 0){
+							System.out.println("\n\t\t\t|MISS");
+							break;
+						}	
+						//Calculate the damage that the attacker will take
+						attackDamage = attacker.strength*ThreadLocalRandom.current().nextInt(3, 5);
+						damageDealt = attackDamage - player.armor;
+						System.out.println("\n\t\t\t|You dealt: " + damageDealt + "DMG");
+						attacker.HP = attacker.HP - damageDealt;
 					
+						//Attacker Move
+						//System.out.println("Attackers Move!");
+						//Miss
+						if(ThreadLocalRandom.current().nextInt(0, 6) == 0){
+							System.out.println("\t\t\t|MISS");
+							break;
+						}	
+						//Calculate the damage that the player will take
+						attackerDamage = attacker.strength*ThreadLocalRandom.current().nextInt(3, 5);
+						damageTaken = attackerDamage - player.armor;
+						System.out.println("\t\t\t|You took:  " + damageTaken + "DMG");
+						player.HP = player.HP - damageTaken;
+						
+						//If damageTaken is less than 0, reset it to 0 so the player doesnt gain hp
+						if(damageTaken < 0) {
+							damageTaken = 0;
+						}
+						break;
+					}
+					
+					
+					//If the Attacker is faster
+					if(player.getAgility() <= attacker.getAgility()) {
+						//System.out.println("Attackers Move!");
+						//Miss
+						if(ThreadLocalRandom.current().nextInt(0, 6) == 0){
+							System.out.println("\t\t\t|MISS");
+							break;
+						}	
+						//Calculate the damage that the player will take
+						attackerDamage = attacker.strength*ThreadLocalRandom.current().nextInt(3, 5);
+						damageTaken = attackerDamage - player.armor;
+						System.out.println("\n\t\t\t|You took:  " + damageTaken + "DMG");
+						player.HP = player.HP - damageTaken;
+						
+						//Player Move
+						//System.out.println("Players Move!");
+						//Miss
+						if(ThreadLocalRandom.current().nextInt(0, 6) == 0){
+							System.out.println("\n\t\t\t|MISS");
+							break;
+						}	
+						//Calculate the damage that the attacker will take
+						attackDamage = attacker.strength*ThreadLocalRandom.current().nextInt(3, 5);
+						damageDealt = attackDamage - player.armor;
+						System.out.println("\t\t\t|You dealt: " + damageDealt + "DMG");
+						attacker.HP = attacker.HP - damageDealt;
+						
+						//If damageTaken is less than 0, reset it to 0 so the player doesnt gain hp
+						if(damageTaken < 0) {
+							damageTaken = 0;
+						}
+						break;
+					}
 					break;
+						
+					
 					
 				case 2:
 					//Miss
@@ -95,12 +162,12 @@ public class Adventure {
 					System.out.println("Invalid option. Please try again.\n"); 
 					break;
 			}
-			if(attacker.getHP() < 0) {
+			if(attacker.getHP() <= 0) {
 				attacker.HP = 0;
 				System.out.print("\t\t\t|You won the fight!\n");
 				winStatus =  false;
 				return winStatus;
-			}else if(player.getHP() < 0) {
+			}else if(player.getHP() <= 0) {
 				player.HP = 0;
 				System.out.print("You lost the fight \t|\n\n");
 				winStatus = true;
