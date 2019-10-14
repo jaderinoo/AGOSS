718248aab1bb78d7951f703c47dd051095c67417
Jad El-Khatib <jaderinoo@gmail.com>
2019-10-14 04:32:02 -0400

    Added a dialogue exist check

--- AGOSS/src/Dialogues.java
+++ AGOSS/src/Dialogues.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.util.ArrayList;
 
@@ -7,21 +8,21 @@ public class Dialogues {
 	// "-" dictates a small pause
 	public static void readDialogue(String mapName) throws Exception {
 		//Checks if dialogue exists
-		if (mapName == null){
-			return;
-		}
-		
-		//Continue if it does
-		ArrayList<String> listOfLines = inputToString("src\\dialogues\\" + mapName + ".txt");
-		
-		//Loop through the list
-		for(int i = 0; i != listOfLines.size(); i++) {
-			//If the item - shows up, skip it and add a delay
-			if(listOfLines.get(i).equals("-")) {
-				Thread.sleep(200);
-			}else {
-				System.out.println(listOfLines.get(i));
+		if (new File("src\\dialogues\\" + mapName + ".txt").exists()){
+			//Continue if it does
+			ArrayList<String> listOfLines = inputToString("src\\dialogues\\" + mapName + ".txt");
+			
+			//Loop through the list
+			for(int i = 0; i != listOfLines.size(); i++) {
+				//If the item - shows up, skip it and add a delay
+				if(listOfLines.get(i).equals("-")) {
+					Thread.sleep(200);
+				}else {
+					System.out.println(listOfLines.get(i));
+				}
 			}
+		}else {
+			System.out.println("Missing dialogue .txt");
 		}
 	}
 	
@@ -36,8 +37,7 @@ public class Dialogues {
 	      listOfLines.add(line);
 	      line = reader.readLine();
 	    }
-	    
+		reader.close();
 		return listOfLines;
-
 	} 
 }
